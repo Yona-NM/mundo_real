253305846ed23f40c55f62cd5f289c75cad6e234
yonathan <yonathan.nahuel@cftaraucania.cl>
2022-07-19 09:25:39 -0400

    se agrega javascript

---
+++ php/datos.php
@@ -0,0 +1,19 @@
+<?php
+
+$fechaingreso=$_POST['fechaingreso'];
+$causa=$_POST['causa'];
+$comuna=$_POST['Comuna'];
+$tribunal=$_POST['Tribunal'];
+$parte=$_POST['Parte'];
+$fechaparte=$_POST['fechaparte'];
+$delito=$_POST['Delito'];
+$funcionario=$_POST['Funcionario'];
+$Unidadiligencia=$_POST['Unidadiligencia'];
+$Tiporden=$_POST['Tiporden'];
+$Plazo=$_POST['plazo'];
+
+echo "los datos son los siguientes= <br>";
+echo "$fechaingreso,$causa,$Comuna,$Tribunal,$Parte,$fechaparte,$Delito,$Funcionario,$Unidadiligencia,$Tiporden y $plazo";
+
+
+?>
